e2430f3cc71b03cd4bed2b6419f8a6e918e2b909
gui <guilherme.deck1227@gmail.com>
2024-04-04 16:00:11 -0300

    clean up Vite styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Outlet />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button style={{ color: "white" }} onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <Outlet />;
 }
 
 export default App;
